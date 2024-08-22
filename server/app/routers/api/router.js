2719ceaea5ab329e601ace1c7143ebf669a10dcf
Dylan Robinaud <dylan.rbnd@gmail.com>
2024-08-22 15:06:24 +0200

    Modification de la base de données et création de la route afin de récupérer un contact

--- server/app/routers/api/router.js
+++ server/app/routers/api/router.js
@@ -13,6 +13,7 @@ router.use("/items", itemsRouter);
 const contactsRouter = require("./contacts/router");
 
 router.use("/contacts", contactsRouter);
+router.use("/contacts/:id", contactsRouter);
 /* ************************************************************************* */
 
 module.exports = router;
